a8d41e3169306c79cf0c2e91bd35ce21cd4a175a
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2025-03-02 22:28:49 +0000

    added main function and notes

--- Mod/factorial.cpp
+++ Mod/factorial.cpp
@@ -8,12 +8,24 @@ CSC 249
 #include <iostream>
 using namespace std;
 
+// calculating factorial of n using recursion
+// FIXME: delete "else" clause?
 int factorial(int n){
-    if (n == 1){
-        return 1
+    if (n == 0 || n == 1){
+        return 1;
     }
     else {
-        return n * factorial(n - 1)
+        return n * factorial(n - 1);
     }
+}
 
+// calculating factorial of n using a for loop
+int factNum(int n){
+    int 
+}
+
+int main(){
+    int num = 5;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
 }
