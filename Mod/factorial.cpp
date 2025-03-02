b2a202e6e00d1484541f2289a7e32e7faf52dd67
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2025-03-02 22:54:05 +0000

    added factNum function, fixed spacing, order

--- Mod/factorial.cpp
+++ Mod/factorial.cpp
@@ -1,12 +1,20 @@
 /*
 Laura K. Jackson
 CSC 249
-2.H1 - Recursive Factorial Search
+2.H1 - Factorial functions: Iterative & Recursive 
 3.2.2025
 */
 
 #include <iostream>
 using namespace std;
+
+// calculating factorial of n using a for loop
+int factNum(int n){
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+        result = result * i;
+    return result;
+}
 
 // calculating factorial of n using recursion
 // FIXME: delete "else" clause?
@@ -19,13 +27,16 @@ int factorial(int n){
     }
 }
 
-// calculating factorial of n using a for loop
-int factNum(int n){
-    int 
-}
-
+// main function
 int main(){
     int num = 5;
+    // iterative (for loop) 
+    cout << "Using a for loop ";
     cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
+    // recursion
+    cout << "Using recursion ";
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
     return 0;
 }
