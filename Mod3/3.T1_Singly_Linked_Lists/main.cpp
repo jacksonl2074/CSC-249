657f1f74f325c6b21145b3a38fe6c0102cdab692
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2025-03-10 00:56:11 +0000

    changed data in nodes (numbers), added comment block at top

--- Mod3/3.T1_Singly_Linked_Lists/main.cpp
+++ Mod3/3.T1_Singly_Linked_Lists/main.cpp
@@ -1,24 +1,30 @@
+/*
+Laura K. Jackson
+CSC 249 - 3.T1 Singly Linked Lists (From 3.6 Zybooks)
+3.9.2025
+*/
+
 #include <iostream>
 #include "singlylinkedlist.h"
 using namespace std;
 
 int main() {
    LinkedList numList;
-   Node* nodeA = new Node(66);
-   Node* nodeB = new Node(99);
-   Node* nodeC = new Node(44);
-   Node* nodeD = new Node(95);
-   Node* nodeE = new Node(42);
-   Node* nodeF = new Node(17);
+   Node* nodeA = new Node(16);
+   Node* nodeB = new Node(93);
+   Node* nodeC = new Node(56);
+   Node* nodeD = new Node(89);
+   Node* nodeE = new Node(32);
+   Node* nodeF = new Node(27);
 
-   numList.Append(nodeB);   // Add 99
-   numList.Append(nodeC);   // Add 44, make the tail
-   numList.Append(nodeE);   // Add 42, make the tail
+   numList.Append(nodeB);   // Add 93
+   numList.Append(nodeC);   // Add 56, make the tail
+   numList.Append(nodeE);   // Add 32, make the tail
 
-   numList.Prepend(nodeA);  // Add 66, make the head
+   numList.Prepend(nodeA);  // Add 16, make the head
 
-   numList.InsertAfter(nodeC, nodeD);  // Insert 95 after 44
-   numList.InsertAfter(nodeE, nodeF);  // Insert 17 after tail (42)
+   numList.InsertAfter(nodeC, nodeD);  // Insert 89 after 56
+   numList.InsertAfter(nodeE, nodeF);  // Insert 27 after tail (32)
 
    // Output list
    cout << "List after adding nodes: ";
