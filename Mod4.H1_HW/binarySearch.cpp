1b58554b458a684037e78cfe11aeca62a6b54944
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2025-04-13 03:52:08 +0000

    Created Mod4.H1_HW folder, created binarySearch.cpp (copied code from binary_search.cpp in Mod folder), and altered code some for user input

---
+++ Mod4.H1_HW/binarySearch.cpp
@@ -0,0 +1,64 @@
+/*
+Laura K. Jackson
+CSC 249
+2.L2 - Binary Search
+2.22.2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int* numbers, int numbersSize, int key) {
+   // Variables to hold the low and high indices of the area being searched. 
+   // Starts with entire range.
+   int low = 0;
+   int high = numbersSize - 1;
+   
+   // Loop until "low" passes "high"
+   while (high >= low) {
+      // Calculate the middle index
+      int mid = (high + low) / 2;
+
+      // Cut the range to either the left or right half,
+      // unless numbers[mid] is the key
+      if (numbers[mid] < key) {
+         low = mid + 1;
+      }
+      else if (numbers[mid] > key) {
+         high = mid - 1;
+      }
+      else {
+         return mid;
+      }
+   }
+   
+   return -1; // not found
+}
+
+int main() {
+   // int numbers[] = { 2.3, 4.5, 7.1, 10.6, 11.7, 32.4, 45.8, 87.9 };
+   // start with empty vector
+   int numbers[] = {};
+   int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+
+   // take user's input for nums in vector
+   cout << "NUMBERS: [" << numbers[0];
+   for (int i = 1; i < numbersSize; i++) {
+      cout << ", " << numbers[i];
+   }
+   cout << "]" << endl;
+      
+   // Prompt for an integer to search for
+   cout << "Enter a value: ";
+   int key = 0.0;
+   cin >> key;
+      
+   int keyIndex = BinarySearch(numbers, numbersSize, key);
+   if (keyIndex == -1) {
+      cout << key << " was not found." << endl;
+   }
+   else {
+      cout << "Found " << key << " at index ";
+      cout << keyIndex << "." << endl;
+   }
+}
