d1ab9bc75af413a69a624c6464972e676f81c01e
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2025-04-13 20:21:40 +0000

    added var's & start while loop

--- Mod4.H1_HW/numberGuesser.cpp
+++ Mod4.H1_HW/numberGuesser.cpp
@@ -10,14 +10,28 @@ CSC 249
 using namespace std;
 
 void numberGuesser (){
+    // variables
+    int low = 0;
+    int high = 99;
+    int numGuesses = 0;             // num to keep track of guesses
+    const int MAX_GUESSES = 5;      // constant var for Max num of guesses
+    char userResponse;     // variable for user's input of =, >, <
+    bool found = false;     // variable to track if computer correctly guessed user's num
+
     // tell user how the game is set up
     cout << "You have chosen the Number Guesser!" << endl;
     cout << "You are going to think of a number between 0 and 99,";
     cout << " and I will try to guess your number in 5 attempts." << endl;
-    cout << "Is your number 50?" << endl;
+    cout << "If I've guessed your number, please type =" << endl;
+    cout << "If I've guessed too low, please type >" << endl;
+    cout << "If I've guessed too high, please type <" << endl;
+    cout << "Is your number 50? " << endl;
 
-    // user enters =, >, or <
-    cin << var;
+    while (numGuesses != MAX_GUESSES && userResponse != found){
+        // user enters =, >, or <
+        cin >> userResponse;
+    }
+    
 
 
 }
